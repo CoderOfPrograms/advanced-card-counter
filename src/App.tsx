63d239b4cdc8ee76224138e7a4aafeb5ec17051e
Prithiv Premkumar <prithiv2047@gmail.com>
2022-11-03 14:49:24 -0500

    Everything is AWESOME

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.scss'
-import { Button } from 'react-bootstrap'
-import SomeComponent from './SomeComponent'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.scss";
+import { Button } from "react-bootstrap";
+import SomeComponent from "./SomeComponent";
 
 function App() {
   return (
     <div className="App">
-      <div></div>
-      <h1>Advanced Card Counter</h1>
+      <div className="bg-orange-600">
+        <h1>Advanced Card Counter</h1>
+      </div>
       <Button>Hello!</Button>
       <SomeComponent></SomeComponent>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
